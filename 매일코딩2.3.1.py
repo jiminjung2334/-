798c008551a0fd5ae980baba0dea874f4b554df2
Jimin Jung <101380950+jiminjung2334@users.noreply.github.com>
2022-05-20 13:44:48 +0900

    Update 매일코딩2.3.1.py

--- 매일코딩2.3.1.py
+++ 매일코딩2.3.1.py
@@ -1,10 +1,10 @@
 def pl_sentence(input):
-    output = 0
+    output = str()
     for word in input.split():
         if word[0] in 'aeiou':
-            output += word + 'way'
+            output = output + word + 'way'
         else:
-            output += word[1:] + word[0] + 'ay'
+            output = output + word[1:] + word[0] + 'ay'
     print(output)
 
 input = ("this is a test")
